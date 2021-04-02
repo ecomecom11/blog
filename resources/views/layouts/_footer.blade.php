85749d4cf4a5a00114a6cbb88285be0fbeb2ef48
Emily <ecomecom11@gmail.com>
2021-04-02 17:44:43 +0000

    Seperate header and footer from layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <img class="brand-icon" src="https://www.logolynx.com/images/logolynx/d9/d9e8ae4cebb48a11f701436962f76c18.png">
+  <a href="https://learnku.com/laravel/courses" target=_blank>
+    Learn and practice 
+  </a>
+
+  <div class="float-right">
+    <a href="/about" >About</a>
+  </div>
+</footer>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,17 +6,12 @@
   </head>
 
   <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container">
-          <a class="navbar-brand" href="/">Blog App</a>
-          <ul class="navbar-nav justify-content-end">
-            <li class="nav-item"><a class="nav-link" href="/help">Help</a></li>
-            <li class="nav-item" ><a class="nav-link" href="#">Log in</a></li>
-          </ul>
-        </div>
-    </nav>
+    @include('layouts._header')
+    
     <div class="container">
+      <div class="offset-md-1 col-md-10">
       @yield('content')
+      @include('layouts._footer')
     </div>
   </body>
 </html>
