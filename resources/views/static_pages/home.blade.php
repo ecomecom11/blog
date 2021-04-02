5a6286920b4f62afc3fedc8c625d973d42d13965
Emily <ecomecom11@gmail.com>
2021-04-02 17:30:37 +0000

    Initialize style

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.default')
+
 @section('content')
-    <h1>Home</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+        What you are seeing now <a href="https://learnku.com/courses/laravel-essential-training">Laravel Course</a> Demo Page
+        </p>
+        <p>
+        Everything starts from here.
+        </p>
+        <p>
+        <a class="btn btn-lg btn-success" href="#" role="button">Register now</a>
+        </p>
+    </div>
 @stop
