e994d985c07cfffda5656ece18e57843b5a7d51b
Emily <ecomecom11@gmail.com>
2021-04-04 10:34:46 +0000

    Create session

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Auth;
+
+class SessionsController extends Controller
+{
+    //
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)){
+            session()->flash('success', 'Welcome back!');
+            return redirect()->route('users.show', [auth::user()]);
+        } else {
+            session()->flash('danger', 'Sorry, your login information is not correct');
+            return redirect()->back()->withInput();
+        }
+    }    
+    
+}
