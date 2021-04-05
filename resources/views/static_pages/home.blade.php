c394150ea79bf93c86c694165b15be78ccab39b8
Emily <ecomecom11@gmail.com>
2021-04-05 17:32:14 +0000

    Blog homepage layout

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -7,6 +7,9 @@
         <section class="status_form">
           @include('shared._status_form')
         </section>
+        <h4>Blog List</h4>
+        <hr>
+        @include('shared._feed')
       </div>
       <aside class="col-md-4">
         <section class="user_info">
@@ -18,7 +21,7 @@
     <div class="jumbotron">
       <h1>Hello Laravel</h1>
       <p class="lead">
-        What you are seeing <a href="https://learnku.com/courses/laravel-essential-training">Laravel </a> Demo Page。
+        What you are seeing <a href="https://learnku.com/courses/laravel-essential-training">Laravel </a> Demo
       </p>
       <p>
         Something starts from here.
